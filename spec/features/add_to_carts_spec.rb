b18e3c00f7e387f0be5210c756a2e5454abec0ab
jayl214 <jay.lin@mail.mcgill.ca>
2017-08-08 21:27:50 +0000

    can add to cart

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,13 +16,17 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "Clicking on add to cart increases cart size" do
+
     visit root_path
 
-    # commented out b/c it's for debugging only
-    save_and_open_screenshot
+    find(".actions", match: :first).click_link('Add')
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_link('My Cart (1)')
+
+    save_screenshot
+
   end
+
 
 end
--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "ProductDetails", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,13 +16,18 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "On click go to product details" do
     visit root_path
 
-    # commented out b/c it's for debugging only
-    save_and_open_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
+    find(".actions", match: :first).click_link('Details')
+
+    expect(page).to have_css '.product-detail'
+
+    save_screenshot
+
   end
+
+
 
 end
